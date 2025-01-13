d06877c445e53027a22d25786aa0a7a319852ff0
jherron4 <jherron4@ut.utm.edu>
2025-01-12 22:24:47 -0600

    Add sum function to calculate sum of numbers from 1 to n

--- main.cpp
+++ main.cpp
@@ -3,9 +3,24 @@
 
 using namespace std;
 
+
+ int sum(int n){
+    int total;
+    for( int i = 0; i < n; i++){
+      total += i;
+  }
+    return total;
+}
 int main() {
 
   cout << "Hello World!" <<endl;
+  int n; 
+  cout << "Enter a number: ";
+  cin >> n;
+  
+  cout << "THe sum of numbers 1 to " << n << " is " << sum(n) <<endl;
+ 
   
   return 0;
 }
+
